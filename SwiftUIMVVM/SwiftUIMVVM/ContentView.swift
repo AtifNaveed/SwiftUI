8aa481c9d3f03877b940da47e015d5553107a5e0
“Atif <atif.gcucs@gmail.com>
2019-08-26 12:41:44 +0400

    SwiftUI MVVM
    SwiftUI with MVVM

---
+++ SwiftUIMVVM/SwiftUIMVVM/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  SwiftUIMVVM
+//
+//  Created by Atif on 25/08/2019.
+//  Copyright © 2019 Atif. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject private var vm = ViewModel()
+
+    var body: some View {
+        VStack {
+            // TODO textfield binding crashed
+            TextField("Enter amount: ", text: $vm.amount)
+            .padding()
+         
+            Picker(selection: $vm.selectedIndex, label: Text("Picker")) {
+                ForEach(0 ..< vm.options.count) {
+                    Text("\(self.vm.options[$0])")
+                }
+            }.padding()
+            
+            Button(action: {
+                self.vm.calculate()
+            }) {
+                Text("Calculate")
+            }.padding()
+
+                    
+            Text("Calculated value is: \(vm.calculatedValue)")
+        }
+
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
